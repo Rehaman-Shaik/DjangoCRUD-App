9678dbad63719b79f550755b373fb69e0a36abaa
Rehaman <shaik.rehamanrp@gmail.com>
2024-03-25 13:05:24 +0530

    added a new feature regarding django templates extend blocks

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name = 'landing_page'),
     path('add/', views.add_users, name='add_users'),
-    path('list_all', views.list_all_users, name='list_all_users'),
-    path('home', views.home_page, name='home_page'),
+    path('list_all/', views.list_all_users, name='list_all_users'),
+    path('home/', views.home_page, name='home_page'),
     path('user/<id>/',views.detail_view, name="user_details"),
     path('update_user/<int:id>/', views.update_user, name='update_user'),
     path('delete_user/<int:id>/', views.delete_user, name='delete_user'),
