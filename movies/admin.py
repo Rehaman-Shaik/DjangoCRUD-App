552232ff1a1e91e6d093469cfd787bf222b3d167
Rehaman <shaik.rehamanrp@gmail.com>
2024-04-01 22:31:19 +0530

    add movies table in models and registered it

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Movies
 # Register your models here.
+
+class MoviesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Movies, MoviesAdmin)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Movies(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(blank=True,null=True)
+    release_date = models.CharField(max_length=30,blank=True,null=True)
+    rating = models.IntegerField(blank=True,null=True)
+    image= models.ImageField(blank=True,null=True)
+    director = models.CharField(max_length=100,blank=True,null=True)
+    language = models.CharField(max_length=50,blank=True,null=True)
+    runtime = models.CharField(max_length=14,blank=True,null=True)
+    
+    def __str__(self):
+        return "name"
+    
